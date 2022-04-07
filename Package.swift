808cc96586d9e290bb5ae082ebeca5d4eb2f8674
Vitor Hugo <vitor.h8@icloud.com>
2022-04-07 14:11:53 -0300

    Added obj-c bridge

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
     targets: [
         .binaryTarget(name: "GroupLink",
                       url: "https://grouplinknetwork.com/releases/ios/latest.zip",
-                      checksum: "68b486bac18093bd45fa1f43fcc64d9c3bb0e8102bf78baf07bc04ab49f13414")
+                      checksum: "614ab01303a5e3c0ec35c1152a725f3e9c199206a65b32aa22a75eef52fa0474")
     ]
 )
