aff94f17e7441b169975434a987581b802bc74fc
Vitor Hugo <vitor.h8@icloud.com>
2022-05-31 13:26:24 -0300

    4.1.15

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
     targets: [
         .binaryTarget(name: "GroupLink",
                       url: "https://grouplinknetwork.com/releases/ios/latest.zip",
-                      checksum: "7bafff40f62723acc471642b418ffc9873cfeffd33cf039d482ac2785b43e330")
+                      checksum: "4a34d358e173af387e59656592883f5f9d041d0060b6ffd1df0b6577c638df36")
     ]
 )
