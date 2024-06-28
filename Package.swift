5d8772d9ac1fb7ea0675514d1ad802e122c0ca64
Vitor Macedo <v.macedo@grouplinknetwork.com>
2024-06-28 15:07:17 -0300

    Release 5.2.7

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "GroupLink",
-                      url: "https://grouplink.s3.us-west-004.backblazeb2.com/ios/GroupLink+5.2.6.zip",
-                      checksum: "b579bc7753198289335c7d528fb182e6d35889fd1626a3a0de07dbb8d4e93a98")
+                      url: "https://grouplink.s3.us-west-004.backblazeb2.com/ios/GroupLink+5.2.7.zip",
+                      checksum: "ec07490447f6114a5f4832090e4763e3a7ba7c19182dad7712751bd3f0f41d33")
     ]
 )
